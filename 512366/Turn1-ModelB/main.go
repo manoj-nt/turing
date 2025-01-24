4aefbcc3a4875403d15fdd10478abb06cdf97130
macn@github.com <manojnegi@ManojNegis-MacBook-Pro.local>
2025-01-24 23:28:19 +0530

    512366: Turn1-ModelB

---
+++ 512366/Turn1-ModelB/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"sync"
+)
+
+type Config struct {
+	Server string
+	Port   int
+}
+
+var (
+	config Config
+	wg     sync.WaitGroup
+)
+
+func readConfigFile(filename string) {
+	defer wg.Done()
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		fmt.Println("Error parsing JSON:", err)
+	}
+}
+
+func main() {
+	wg.Add(1)
+	go readConfigFile("config.json")
+	wg.Wait()
+
+	fmt.Println("Server:", config.Server)
+	fmt.Println("Port:", config.Port)
+}
